b2599127d328002077ee9e0c18cd83602fb1215b
Ubirajara Neves <ubirajara.neves@usp.br>
2022-07-10 12:57:34 -0300

    Exception and stack

---
+++ inherited-account-bb/src/StackAnalysis.java
@@ -0,0 +1,35 @@
+
+public class StackAnalysis {
+
+	public static void main(String[] args) {
+
+		System.out.println("Main init");
+		method1();
+		System.out.println("Main end");
+
+	}
+	
+	public static void method1() {
+		
+		System.out.println("Method1 init");
+		method2();
+		System.out.println("Method1 end");
+		
+	}
+	
+	public static void method2() {
+		
+		System.out.println("Method2 init");
+		
+		for (int i = 1; i <= 5; i++) {
+		
+			System.out.println(i);
+			int a = i / 0;
+			
+		}
+		
+		System.out.println("Method2 end");
+		
+	}
+
+}
