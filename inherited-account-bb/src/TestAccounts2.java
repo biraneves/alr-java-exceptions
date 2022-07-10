07032a2a694e824b4a6ca0ebd80dd212a36863e9
Ubirajara Neves <ubirajara.neves@usp.br>
2022-07-10 11:44:25 -0300

    Example: NullPointerException

---
+++ inherited-account-bb/src/TestAccounts2.java
@@ -0,0 +1,20 @@
+
+public class TestAccounts2 {
+
+	public static void main(String[] args) {
+
+		// This instance intends to throw an exception
+		CheckingAccount account = null;
+		account.deposit(200.0);
+		
+		// CheckingAccount instance
+		CheckingAccount ca = new CheckingAccount(111, 111);
+		ca.deposit(100.0);
+		
+		// SavingsAccount instance
+		SavingsAccount sa = new SavingsAccount(222, 222);
+		sa.deposit(200.0);
+
+	}
+
+}
