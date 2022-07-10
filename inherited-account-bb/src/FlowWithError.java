78ed4513faa6b12156bcc023646190d917dc1f58
Ubirajara Neves <ubirajara.neves@usp.br>
2022-07-10 14:08:33 -0300

    Generating a StackOverflowError.

---
+++ inherited-account-bb/src/FlowWithError.java
@@ -0,0 +1,37 @@
+
+public class FlowWithError {
+
+	public static void main(String[] args) {
+
+		System.out.println("Main init");
+		
+		try {
+			method1();
+		} catch (ArithmeticException | NullPointerException | MyException e) {
+			String msg = e.getMessage();
+			e.printStackTrace();
+		}
+		
+		System.out.println("Main end");
+		
+	}
+	
+	public static void method1() {
+		
+		System.out.println("Method1 init");
+		method2();
+		System.out.println("Mehotd2 end");
+		
+	}
+	
+	public static void method2() {
+		
+		System.out.println("Method2 init");
+		
+		method2();
+		
+		System.out.println("Method2 end");
+		
+	}
+
+}
