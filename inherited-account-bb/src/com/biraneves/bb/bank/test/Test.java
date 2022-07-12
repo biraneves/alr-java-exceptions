52efd37186c701571d294c4a4baa4b5a778dabf8
Ubirajara Neves <ubirajara.neves@usp.br>
2022-07-11 22:27:21 -0300

    Wrapper, autoboxing, unboxing and parsing.

--- inherited-account-bb/src/com/biraneves/bb/bank/test/Test.java
+++ inherited-account-bb/src/com/biraneves/bb/bank/test/Test.java
@@ -7,15 +7,22 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		int [] ages = new int[5];
-		String [] names = new String[5];
-		List numbers = new ArrayList();
-//		List<int> numbers = new ArrayList<int>(); ==> It doesn't work!
-//		List<Integer> numbers = new ArrayList<Integer>(); ==> It works!
+		int[] ages = new int[5];
+		String[] names = new String[5];
 		
-		int age = 29;	// Integer
-		Integer ageRef = new Integer(29);	// This is deprecated, but it was equivalent to the declaration above
-		numbers.add(age);	// Autoboxing
+		int age = 29;
+		
+		Integer ageRef = Integer.valueOf(age);
+//		int value = ageRef.intValue();	// 
+		System.out.println(ageRef.doubleValue());
+
+		String s = args[0];
+//		Integer number = Integer.valueOf(s);
+		int number = Integer.parseInt(s);
+		System.out.println(number);	// Parsing
+		
+		List<Integer> numbers = new ArrayList<Integer>();
+		numbers.add(29);		// Autoboxing
 		
 	}
 
