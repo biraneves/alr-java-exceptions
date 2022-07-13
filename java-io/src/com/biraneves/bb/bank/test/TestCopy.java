aa426a8cd8b8930d534b9aa89994c85f024b5108
biraneves <ubirajara.neves@usp.br>
2022-07-13 12:09:02 -0300

    Copying a text file.

---
+++ java-io/src/com/biraneves/bb/bank/test/TestCopy.java
@@ -0,0 +1,44 @@
+package com.biraneves.bb.bank.test;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+public class TestCopy {
+
+	public static void main(String[] args) throws IOException {
+
+		// Input stream
+		InputStream fis = new FileInputStream("lorem.txt");
+		Reader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		
+		// Output stream
+		OutputStream fos = new FileOutputStream("lorem2.txt");
+		Writer osw = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		String line = br.readLine();
+		
+		while (line != null && !line.isEmpty()) {
+			
+			bw.write(line);
+			bw.newLine();
+			line = br.readLine();
+			
+		}
+		
+		br.close();
+		bw.close();
+
+	}
+
+}
